5358e64e6855ba38fcf28457cdcfba3e72159d49
XJY <jiayi_xie_xjtu@outlook.com>
2025-11-29 17:58:12 +0800

    it has merge in ros now

--- src/lite3_command.cpp
+++ src/lite3_command.cpp
@@ -7,11 +7,12 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/TwistStamped.h>
 #include <geometry_msgs/Vector3Stamped.h>
-#include <quadrotor_msgs/PositionCommand.h>
 
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "lite3_ego_udp");
+    ros::NodeHandle nh("~");
     // 配置服务器IP和端口
     // wireless
     // const string SERVER_IP = "192.168.2.1";
@@ -27,19 +28,19 @@ int main() {
     }
 
     try {
-        cout << "Starting to send commands..." << endl;
-        while (true) {
+        ROS_INFO("Starting to send commands...");
+        ros::Rate rate(200); // ~200Hz, similar to 5ms sleeps
+        while (ros::ok()) {
             // 发送自动模式指令
             sender.send_auto_mode();
-            usleep(5000); // 5ms
-            // 发送ComplexCmd（速度1.0）
+            // 发送ComplexCmd
             sender.send_complex_cmd(-0.5);    // 旋转
-            sender.send_complex_cmd(0.2, 0x145);   // 左右平移
-            // sender.send_complex_cmd(0.2, 0x140);    // 前后平移
-            usleep(5000); // 5ms
+            // sender.send_complex_cmd(0.2, 0x145);   // 左右平移
+            ros::spinOnce();
+            rate.sleep();
         }
     } catch (const exception& e) {
-        cerr << "Error: " << e.what() << endl;
+        ROS_ERROR("Error in sender loop: %s", e.what());
     }
 
     // 停止所有指令（程序退出前）
